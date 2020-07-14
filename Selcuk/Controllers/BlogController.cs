c3abe134d9a2508c8a53a866fc740e279fae5613
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2020-07-14 13:01:11 +0300

    Layout oluşturuldu.

---
+++ Selcuk/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Selcuk.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Narail|Blog";
+            return View();
+        }
+    }
+}
