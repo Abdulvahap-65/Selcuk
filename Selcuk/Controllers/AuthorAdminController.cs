78b6719d6fe5cb547cf4ac47f982d1a6c8a3275a
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2020-07-18 13:25:32 +0300

    Aauthor Admin kısmı eklendi.

---
+++ Selcuk/Controllers/AuthorAdminController.cs
@@ -0,0 +1,19 @@
+﻿using Selcuk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Selcuk.Controllers
+{
+    public class AuthorAdminController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+          NarailDBEntities db = new NarailDBEntities();
+            return View(db.Author.ToList());
+        }
+    }
+}
